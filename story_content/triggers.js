71aae3f7836ad93d1972ea91d9e75c759a0edab3
javidsyed <javidsyed.ae@gmail.com>
2026-08-09 18:40:58 +0400

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,90 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5nXqVzExfZD":
+        Script1();
+        break;
+      case "6qx2m4nLTMO":
+        Script2();
+        break;
+      case "5k8jXAftCBC":
+        Script3();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+var getKeyDown = player.getKeyDown;
+var keydown = player.keydown;
+var keyup = player.keyup;
+window.Script1 = function()
+{
+  const target = object('6Cwv2fLGZrm');
+const duration = 2500;
+const easing = 'ease-out';
+const id = '5nfviLlFErH';
+const growAmount = 0.2;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 + growAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script2 = function()
+{
+  const target = object('6F4D1OSLXj7');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6HWspBRHIOa';
+const growAmount = 0.2;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 + growAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script3 = function()
+{
+  const target = object('6UXKNCW2KyC');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6OwcKiOQn6w';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
